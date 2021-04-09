1c859a75204816791d6734be4903d5046ffd066e
Vali <56320456+JoggingMist@users.noreply.github.com>
2021-04-10 01:56:55 +0300

    adaugare canvas si star rating

--- js/slideLogo.js
+++ js/slideLogo.js
@@ -2,20 +2,19 @@ var id = null;
 function slideLogo(){
     var elem = document.getElementById("slideLogo");
     var elem2 = document.getElementById("canvas");
-    var pos = 50;
-    var op = 0;
+    var elem3 = document.getElementById("star-wrapper");
+    var pos = 60;
     clearInterval(id);
     id = setInterval(frame, 10);
     function frame(){
         if(pos == 90){
             elem.style.animation = 0 + 's';
             clearInterval(id);
+            elem2.style.visibility = "visible";
         }
-        else {
+        else{
             pos++;
-            op+=2;
             elem.style.bottom = pos + '%';
-            elem2.style.opacity = op + '%';
         }
     }
 }
